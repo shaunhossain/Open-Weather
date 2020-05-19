ab11ecc3c3fe89b9fbe917542572fb831e0db504
shaunhossain <shaunhossain95@gmail.com>
2020-05-19 11:47:12 +0600

    open weather apps with retrofit

--- app/src/main/java/com/shaunhossain/openweather/WeatherFragment.kt
+++ app/src/main/java/com/shaunhossain/openweather/WeatherFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class WeatherFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = WeatherFragment()
-    }
+    //private lateinit var viewModel: WeatherViewModel
+    private var weatherData: TextView? = null
+    private var weatherButton: Button? = null
 
-    private lateinit var viewModel: WeatherViewModel
+    companion object {
+        var BaseUrl = "http://api.openweathermap.org/"
+        var AppId = "2e65127e909e178d0af311a81f39948c"
+        var lat = "35"
+        var lon = "139"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +35,58 @@ class WeatherFragment : Fragment() {
         return inflater.inflate(R.layout.weather_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        weatherData = view.findViewById(R.id.textView)
+
+        weatherButton = view.findViewById(R.id.button)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+        //viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
+        weatherButton?.setOnClickListener { getCurrentData() }
     }
+
+    internal fun getCurrentData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(WeatherService::class.java)
+        val call = service.getCurrentWeatherData(lat, lon, AppId)
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main!!.pressure
+
+                    weatherData!!.text = stringBuilder
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                weatherData!!.text = t.message
+            }
+        })
+    }
+
 
 }
